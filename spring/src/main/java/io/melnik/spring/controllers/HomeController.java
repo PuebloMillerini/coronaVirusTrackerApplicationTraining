f5bc331990ebc4a061b35db98c503ca0737ea23e
PuebloMillerini <44727319+PuebloMillerini@users.noreply.github.com>
2021-11-08 14:16:34 +0100

    Add files via upload

---
+++ spring/src/main/java/io/melnik/spring/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package io.melnik.spring.controllers;
+
+import io.melnik.spring.models.LocationStats;
+import io.melnik.spring.services.CorornaVirusDataService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    @Autowired
+    CorornaVirusDataService corornaVirusDataService;
+
+    @GetMapping("/")
+    public String home(Model model) {
+        List<LocationStats> allStats = corornaVirusDataService.getAllStats();
+        int totalReportedCases = allStats.stream().mapToInt(stat -> stat.getLatestTotalCases()).sum();
+        int totalNewCases = allStats.stream().mapToInt(stat -> stat.getDiffFromPrevDay()).sum();
+        model.addAttribute("locationStats", allStats);
+        model.addAttribute("totalReportedCases", totalReportedCases);
+        model.addAttribute("totalNewCases", totalNewCases);
+
+        return "home";
+    }
+
+}
